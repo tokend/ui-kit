d8339d086de964cc773f0f9760c55efec8187a83
eduard20028 <edikkolesnik02@gmail.com>
2020-10-09 15:17:25 +0300

    Beautify Photo Field

--- src/components/UiPhoto/PhotoField.vue
+++ src/components/UiPhoto/PhotoField.vue
@@ -1,81 +1,74 @@
 <template>
   <div class="ui-photo-field">
-    <div
-      class="ui-photo-field__label"
-      :class="{
-        'ui-photo-field__label--disabled': $attrs.disabled,
-        'ui-photo-field__label--error': errorMessage,
-      }"
-    >
-      <slot name="mainLabel">
-        Your photo
-      </slot>
-    </div>
-
-    <div
-      class="ui-photo-field__content"
-      :class="{
-        'ui-photo-field__content--disabled': $attrs.disabled,
-        'ui-photo-field__content--error': errorMessage,
-        'ui-photo-field__content--highlighted': isFileDragged,
-      }"
-    >
-      <template v-if="document">
-        <div
-          v-if="documentUrl && isImageSelected"
-          class="ui-photo-field__img-preview-wrp"
-          @click="reset"
-        >
-          <img
-            class="ui-photo-field__img-preview"
-            :src="documentUrl"
+    <div class="ui-photo-field__container">
+      <div
+        class="ui-photo-field__content"
+        :class="{
+          'ui-photo-field__content--disabled': $attrs.disabled,
+          'ui-photo-field__content--error': errorMessage,
+          'ui-photo-field__content--highlighted': isFileDragged,
+        }"
+      >
+        <template v-if="document">
+          <div
+            v-if="documentUrl && isImageSelected"
+            class="ui-photo-field__img-preview-wrp"
           >
-        </div>
+            <img class="ui-photo-field__img-preview" :src="documentUrl" />
+          </div>
 
-        <div
-          v-else
-          class="ui-photo-field__icon-preview-wrp">
-          <i
-            class="mdi mdi-file ui-photo-field__icon-preview" />
-        </div>
-      </template>
+          <div v-else class="ui-photo-field__icon-preview-wrp">
+            <i class="mdi mdi-file ui-photo-field__icon-preview" />
+          </div>
 
-      <div class="ui-photo-field__inner">
-        <template v-if="!$attrs.disabled">
-          <i
-            v-if="!document"
-            class="mdi ui-photo-field__icon mdi-camera"
-          />
+          <div class="ui-photo-field__change-preview-wrp">
+            <span class="ui-photo-field__change-preview">Change photo</span>
+          </div>
         </template>
+
+        <div class="ui-photo-field__inner">
+          <template v-if="!$attrs.disabled">
+            <i v-if="!document" class="mdi ui-photo-field__icon mdi-camera" />
+            <div
+              v-if="!document"
+              class="ui-photo-field__label"
+              :class="{
+                'ui-photo-field__label--disabled': $attrs.disabled,
+                'ui-photo-field__label--error': errorMessage,
+              }"
+            >
+              <slot name="mainLabel"> Your photo </slot>
+            </div>
+          </template>
+        </div>
+
+        <input
+          v-bind="$attrs"
+          type="file"
+          class="ui-photo-field__input"
+          :accept="acceptedExtensions"
+          title=""
+          @click="reset"
+          @change="onChange"
+          @dragenter="isFileDragged = true"
+          @dragleave="isFileDragged = false"
+          @drop="isFileDragged = false"
+        />
       </div>
 
-      <input
-        v-bind="$attrs"
-        type="file"
-        class="ui-photo-field__input"
-        :accept="acceptedExtensions"
-        title=""
-        @change="onChange"
-        @dragenter="isFileDragged = true"
-        @dragleave="isFileDragged = false"
-        @drop="isFileDragged = false"
-      >
-    </div>
-
-    <div
-      class="ui-photo-field__err-mes"
-      v-if="hasSlot('error')">
-      <slot name="error" />
+      <div class="ui-photo-field__err-mes" v-if="hasSlot('error')">
+        <slot name="error" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { FileUtil, FileNotPresentInEventError } from '../../utils/file.util'
-import { Document } from '@tokend/js-sdk'
+import { FileUtil, FileNotPresentInEventError } from "../../utils/file.util";
+import { Document } from "@tokend/js-sdk";
 
-const MAX_FILE_MEGABYTES = 32
-const FILE_EXTENSIONS = ['jpg', 'png', 'jpeg']
+const MAX_FILE_MEGABYTES = 32;
+const FILE_EXTENSIONS = ["jpg", "png", "jpeg"];
 
 export default {
   props: {
@@ -85,7 +78,7 @@ export default {
     },
     documentType: {
       type: String,
-      default: 'default',
+      default: "default",
     },
     fileExtensions: {
       type: Array,
@@ -112,127 +105,124 @@ export default {
   data: (_) => ({
     document: null,
     isFileDragged: false,
-    documentUrl: '',
+    documentUrl: "",
   }),
 
   computed: {
-    maxSizeBytes () {
-      return this.maxSize * 1024 * 1024
+    maxSizeBytes() {
+      return this.maxSize * 1024 * 1024;
     },
 
-    isImageSelected () {
-      const mimeType = this.document.mimeType
-      return mimeType && mimeType.includes('image')
+    isImageSelected() {
+      const mimeType = this.document.mimeType;
+      return mimeType && mimeType.includes("image");
     },
 
-    acceptedExtensions () {
+    acceptedExtensions() {
       return this.fileExtensions
         .map((item) => `.${item.toUpperCase()}`)
-        .join(', ')
+        .join(", ");
     },
   },
   watch: {
     value: {
-      handler (value) {
-        this.document = value
-        if (value) this.loadDocumentUrl(value)
+      handler(value) {
+        this.document = value;
+        if (value) this.loadDocumentUrl(value);
       },
       immediate: true,
     },
   },
   methods: {
-    hasSlot (slot) {
-      return !!this.$slots[slot]
+    hasSlot(slot) {
+      return !!this.$slots[slot];
     },
-    async loadDocumentUrl (document) {
+    async loadDocumentUrl(document) {
       if (document.key) {
-        this.documentUrl = await document.getPrivateUrl()
+        this.documentUrl = await document.getPrivateUrl();
       } else {
-        this.documentUrl = await FileUtil.getDataUrl(document.file)
+        this.documentUrl = await FileUtil.getDataUrl(document.file);
       }
     },
-    async onChange (event) {
+    async onChange(event) {
       try {
-        const file = FileUtil.getFileFromEvent(event)
+        const file = FileUtil.getFileFromEvent(event);
         if (await this.validateFile(file)) {
-          this.documentUrl = await FileUtil.getDataUrl(file)
+          this.documentUrl = await FileUtil.getDataUrl(file);
           this.document = new Document({
             mimeType: file.type,
             type: this.documentType,
             name: file.name,
             file: file,
-          })
-          this.$emit('input', this.document)
+          });
+          this.$emit("input", this.document);
         } else {
-          this.$emit('error')
+          this.$emit("error");
         }
       } catch (e) {
         if (e instanceof FileNotPresentInEventError) {
-          this.reset()
+          this.reset();
         } else {
-          this.$emit(e)
+          this.$emit(e);
         }
-        return false
+        return false;
       }
     },
 
-    reset () {
-      this.$el.querySelector('input').value = ''
-      this.document = null
-      this.documentUrl = ''
-      this.$emit('input', this.document)
+    reset() {
+      this.$el.querySelector("input").value = "";
+      this.document = null;
+      this.documentUrl = "";
+      this.$emit("input", this.document);
     },
 
-    async validateFile (file) {
+    async validateFile(file) {
       if (!this.isValidFileType(file)) {
-        this.$emit('err-invalid-file-type')
-        return false
+        this.$emit("err-invalid-file-type");
+        return false;
       }
 
       if (!this.isValidFileSize(file)) {
-        this.$emit('err-invalid-file-size')
-        return false
+        this.$emit("err-invalid-file-size");
+        return false;
       }
 
       if (!(await this.isValidFileDimensions(file))) {
-        this.$emit('err-invalid-file-dimensions')
-        return false
+        this.$emit("err-invalid-file-dimensions");
+        return false;
       }
-      return true
+      return true;
     },
 
-    async isValidFileDimensions (file) {
-      if (!file.type.includes('image')) {
-        return true
+    async isValidFileDimensions(file) {
+      if (!file.type.includes("image")) {
+        return true;
       }
       try {
-        const img = await FileUtil.readImage(file)
-        return img.width >= this.minWidth && img.height >= this.minHeight
+        const img = await FileUtil.readImage(file);
+        return img.width >= this.minWidth && img.height >= this.minHeight;
       } catch (e) {
-        return false
+        return false;
       }
     },
 
-    isValidFileType (file) {
+    isValidFileType(file) {
       return Boolean(
         this.fileExtensions.find(
           (item) => item.toUpperCase() === this.getFileExtension(file)
         )
-      )
+      );
     },
 
-    getFileExtension (file) {
-      return file.name
-        .split('.')
-        .pop()
-        .toUpperCase()
+    getFileExtension(file) {
+      return file.name.split(".").pop().toUpperCase();
     },
 
-    isValidFileSize (file) {
-      return file.size <= this.maxSizeBytes
+    isValidFileSize(file) {
+      return file.size <= this.maxSizeBytes;
     },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -270,14 +260,21 @@ $z-reset-btn: 1;
     line-height: 1;
   }
 
+  &__inner {
+    display: var(--ui-display);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
   &__input {
     position: var(--ui-position);
     top: var(--ui-top);
     left: var(--ui-left);
     cursor: var(--ui-cursor);
     opacity: 0;
-    height: 100%;
     width: var(--ui-width);
+    height: 100%;
   }
 
   &__icon {
@@ -289,11 +286,15 @@ $z-reset-btn: 1;
     color: var(--ui-col-error);
   }
 
+  &__container {
+    display: var(--ui-display);
+    align-items: center;
+    flex-direction: column;
+  }
+
   &__content {
     display: var(--ui-display);
     justify-content: center;
-    align-items: center;
-    margin-top: 1rem;
     width: 15rem;
     height: 15rem;
     position: relative;
@@ -304,6 +305,10 @@ $z-reset-btn: 1;
     &:not(.file-field__content--disabled):hover {
       border-color: var(--ui-content-border-color);
     }
+  }
+
+  &__content:hover &__change-preview-wrp {
+    opacity: 1;
   }
 
   &__content--error {
@@ -338,6 +343,21 @@ $z-reset-btn: 1;
 
   &__icon-preview {
     font-size: 8rem;
+  }
+
+  &__change-preview-wrp {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    color: var(--ui-label-color);
+    font-size: var(--ui-font-size);
+    border-radius: 50%;
+    background: var(--ui-content-background-color);
+    transition: 0.5s;
+    opacity: 0;
   }
 
   &__err-mes {
--- src/components/UiPhoto/index.vue
+++ src/components/UiPhoto/index.vue
@@ -2,7 +2,6 @@
   <div class="ui-photo">
     <div class="ui-photo-clipper">
       <ui-photo-field
-        v-if="!isEditorOpened"
         name="ui-photo-clipper-img"
         :value="originImg"
         :document-type="documentType"
@@ -18,80 +17,86 @@
         @err-invalid-file-dimensions="$emit('err-invalid-file-dimensions')"
       />
 
-      <template v-if="isEditorOpened">
-        <div
-          class="ui-photo-clipper__label"
-          :class="{
-            'ui-photo-clipper__label--disabled': $attrs.disabled,
-            'ui-photo-clipper__label--error': errorMessage,
-          }"
-        >
-          <slot name="mainLabel" />
-        </div>
+      <transition name="ui-photo-clipper__modal" v-if="isEditorOpened">
+        <div class="ui-photo-clipper__modal-mask">
+          <div class="ui-photo-clipper__modal-wrapper">
+            <div class="ui-photo-clipper__modal-container">
+              <div class="ui-photo-clipper__modal-body">
+                <div
+                  class="ui-photo-clipper__label"
+                  :class="{
+                    'ui-photo-clipper__label--disabled': $attrs.disabled,
+                    'ui-photo-clipper__label--error': errorMessage,
+                  }"
+                >
+                  <slot name="mainLabel" />
+                </div>
 
-        <div class="ui-photo-clipper__editor">
-          <clipper-basic
-            class="ui-photo-clipper__clipper"
-            :src="imgURL"
-            ref="clipper"
-            bg-color="transparent"
-            :ratio="ratio"
-          >
-            <div
-              class="ui-photo-clipper__no-image"
-              slot="placeholder">
-              ƒ
-              <!-- eslint-disable max-len -->
-              <i
-                class="mdi mdi-face-recognition ui-photo-clipper__icon ui-photo-clipper__reset-icon"
-              />
-              <!-- eslint-enable max-len -->
+                <div class="ui-photo-clipper__editor">
+                  <clipper-basic
+                    class="ui-photo-clipper__clipper"
+                    :src="imgURL"
+                    ref="clipper"
+                    bg-color="transparent"
+                    :ratio="ratio"
+                  >
+                    <div class="ui-photo-clipper__no-image" slot="placeholder">
+                      ƒ
+                      <!-- eslint-disable max-len -->
+                      <i
+                        class="mdi mdi-face-recognition ui-photo-clipper__icon ui-photo-clipper__reset-icon"
+                      />
+                      <!-- eslint-enable max-len -->
+                    </div>
+                  </clipper-basic>
+
+                  <div class="ui-photo-clipper__actions">
+                    <button
+                      class="app__button-flat"
+                      type="button"
+                      v-if="!$attrs.disabled"
+                      :title="'clipper-field.save-lbl'"
+                      :disabled="disabled"
+                      @click="cropImg"
+                    >
+                      <slot name="saveDragedPhoto" />
+                    </button>
+                    <button
+                      class="app__button-flat ui-photo-clipper__reset-btn"
+                      type="button"
+                      v-if="!$attrs.disabled"
+                      :title="'clipper-field.cancel-lbl'"
+                      :disabled="disabled"
+                      @click="reset"
+                    >
+                      <slot name="cancelSavingDragedPhoto" />
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-          </clipper-basic>
-
-          <div class="ui-photo-clipper__actions">
-            <button
-              class="app__button-flat"
-              type="button"
-              v-if="!$attrs.disabled"
-              :title="'clipper-field.save-lbl'"
-              :disabled="disabled"
-              @click="cropImg"
-            >
-              <slot name="saveDragedPhoto" />
-            </button>
-            <button
-              class="app__button-flat ui-photo-clipper__reset-btn"
-              type="button"
-              v-if="!$attrs.disabled"
-              :title="'clipper-field.cancel-lbl'"
-              :disabled="disabled"
-              @click="reset"
-            >
-              <slot name="cancelSavingDragedPhoto" />
-            </button>
           </div>
         </div>
-      </template>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import PhotoField from './PhotoField'
-import { clipperBasic } from 'vuejs-clipper'
-import Vue from 'vue'
-import VueRx from 'vue-rx'
-import { Document } from '@tokend/js-sdk'
+import PhotoField from "./PhotoField";
+import { clipperBasic } from "vuejs-clipper";
+import Vue from "vue";
+import VueRx from "vue-rx";
+import { Document } from "@tokend/js-sdk";
 
-Vue.use(VueRx)
+Vue.use(VueRx);
 
-const MAX_FILE_MEGABYTES = 32
-const IMAGE_FILE_EXTENSIONS = ['jpg', 'png', 'jpeg']
+const MAX_FILE_MEGABYTES = 32;
+const IMAGE_FILE_EXTENSIONS = ["jpg", "png", "jpeg"];
 
 export default {
   components: {
-    'ui-photo-field': PhotoField,
+    "ui-photo-field": PhotoField,
     clipperBasic,
   },
   props: {
@@ -101,7 +106,7 @@ export default {
     },
     documentType: {
       type: String,
-      default: 'default',
+      default: "default",
     },
     maxSize: {
       type: Number,
@@ -129,77 +134,82 @@ export default {
     },
   },
 
-  data () {
+  data() {
     return {
       IMAGE_FILE_EXTENSIONS,
       originImg: null,
-      imgURL: '',
-      resultURL: '',
+      imgURL: "",
+      resultURL: "",
       isEditorOpened: false,
-    }
+    };
   },
-  created () {
-    this.originImg = this.value
+  created() {
+    this.originImg = this.value;
   },
   methods: {
-    tryCropImg (value) {
+    tryCropImg(value) {
       if (value) {
-        this.originImg = value
-        if (this.imgURL) URL.revokeObjectURL(this.imgURL)
-        this.imgURL = window.URL.createObjectURL(value.file)
-        this.isEditorOpened = true
+        this.originImg = value;
+        if (this.imgURL) URL.revokeObjectURL(this.imgURL);
+        this.imgURL = window.URL.createObjectURL(value.file);
+        this.isEditorOpened = true;
       } else {
-        this.reset()
+        this.reset();
       }
     },
-    async cropImg () {
-      const canvas = this.$refs.clipper.clip() // call component's clip method
-      this.resultURL = canvas.toDataURL(this.originImg.mimeType, 1)
+    async cropImg() {
+      const canvas = this.$refs.clipper.clip(); // call component's clip method
+      this.resultURL = canvas.toDataURL(this.originImg.mimeType, 1);
 
-      const blob = this.dataURItoBlob(this.resultURL)
+      const blob = this.dataURItoBlob(this.resultURL);
       this.originImg = new Document({
         mimeType: blob.type,
-        name: 'new__' + this.originImg.name,
+        name: "new__" + this.originImg.name,
         type: this.documentType,
         file: blob,
-      })
-      this.isEditorOpened = false
-      this.$emit('input', this.originImg)
+      });
+      this.isEditorOpened = false;
+      this.$emit("input", this.originImg);
     },
-    reset () {
-      if (this.imgURL) URL.revokeObjectURL(this.imgURL)
-      this.imgURL = ''
-      this.originImg = null
-      this.isEditorOpened = false
-      this.$emit('input', this.originImg)
+    reset() {
+      if (this.imgURL) URL.revokeObjectURL(this.imgURL);
+      this.imgURL = "";
+      this.originImg = null;
+      this.isEditorOpened = false;
+      this.$emit("input", this.originImg);
     },
-    dataURItoBlob (dataURI) {
+    dataURItoBlob(dataURI) {
       // convert base64 to raw binary data held in a string
       // doesn't handle URLEncoded DataURIs - see SO answer
       // #6850276 for code that does this
-      let byteString = atob(dataURI.split(',')[1])
-      let mimeString = dataURI
-        .split(',')[0]
-        .split(':')[1]
-        .split(';')[0]
-      let ab = new ArrayBuffer(byteString.length)
-      let ia = new Uint8Array(ab)
+      let byteString = atob(dataURI.split(",")[1]);
+      let mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+      let ab = new ArrayBuffer(byteString.length);
+      let ia = new Uint8Array(ab);
 
       for (let i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i)
+        ia[i] = byteString.charCodeAt(i);
       }
       return new Blob([ab], {
         type: mimeString,
-      })
+      });
     },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "../../styles/all";
 
 $max-width: 40rem;
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
 
 .ui-photo-clipper {
   --ui-display: flex;
@@ -299,6 +309,63 @@ $max-width: 40rem;
     display: var(--ui-display);
     justify-content: var(--ui-justify-content);
     padding: 2rem;
+  }
+
+  &__modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
+  }
+
+  &__modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  &__modal-container {
+    width: 50rem;
+    margin: 0px auto;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+
+  &__modal-header h3 {
+    margin-top: 0;
+    text-align: center;
+    text-transform: uppercase;
+    color: var(--ui-text-alight);
+  }
+
+  &__modal-body {
+    margin: 20px 0;
+  }
+
+  &__modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+
+  &__modal-enter &__modal-container,
+  &__modal-leave-active &__modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+
+  &__modal-enter {
+    opacity: 0;
+  }
+
+  &__modal-leave-active {
+    opacity: 0;
   }
 }
 </style>
